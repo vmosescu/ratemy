45af9d8e0f1ad1d05687196d00a16d6a49217df6
vmosescu <vmosescu@hotmail.com>
2024-11-17 04:30:59 +0200

    added firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:flutter/material.dart';
 import 'dart:ui';
 
-import 'package:fashion/screens/feed.dart';
-import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+import 'package:fashion/screens/auth.dart';
+import 'package:fashion/screens/feed.dart';
+import 'package:fashion/screens/splash.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -17,9 +27,20 @@ class MyApp extends StatelessWidget {
       title: 'Fashion App',
       scrollBehavior: MyCustomScrollBehavior(),
       theme: ThemeData().copyWith(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const FeedScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+          if (snapshot.hasData) {
+            return const FeedScreen();
+          }
+          return const AuthScreen();
+        },
+      ),
     );
   }
 }
@@ -27,10 +48,9 @@ class MyApp extends StatelessWidget {
 class MyCustomScrollBehavior extends MaterialScrollBehavior {
   // Override behavior methods and getters like dragDevices
   @override
-  Set<PointerDeviceKind> get dragDevices => { 
-    PointerDeviceKind.touch,
-    PointerDeviceKind.mouse,
-    // etc.
-  };
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        // etc.
+      };
 }
-
