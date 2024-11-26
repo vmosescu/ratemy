ba401ac77948d668243e8caa4a74e444a54ccd2d
vmosescu <vmosescu@hotmail.com>
2024-11-26 16:37:52 +0200

    username

--- lib/widgets/ratemy_list.dart
+++ lib/widgets/ratemy_list.dart
@@ -1,13 +1,11 @@
-import 'dart:async';
-
+import 'package:fashion/widgets/ratemy.dart';
 import 'package:flutter/material.dart';
 
-import 'package:firebase_storage/firebase_storage.dart';
-
-import 'package:fashion/widgets/ratemy.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class RatemyList extends StatefulWidget {
   const RatemyList({super.key});
+
   @override
   State<RatemyList> createState() {
     return _RatemyListState();
@@ -15,61 +13,44 @@ class RatemyList extends StatefulWidget {
 }
 
 class _RatemyListState extends State<RatemyList> {
-  final _photosRef = FirebaseStorage.instance.ref().child('photos');
-  String? _token;
-  final _controller = StreamController<ListResult>();
-  var _indexer = 0;
-
-  void listPhotos() async {
-    final listResult = await _photosRef.list(ListOptions(
-      maxResults: 2,
-      pageToken: _token,
-    ));
-    print('$_token');
-    _token = listResult.nextPageToken;
-    _controller.add(listResult);
-  }
-
-  void loadPhotos() {
-    print('addItems');
-    _indexer = 0;
-    listPhotos();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    loadPhotos();
-  }
-
   @override
   Widget build(BuildContext context) {
-    Widget noContent(String msg) {
-      return Center(child: Text(msg));
-    }
-
     return StreamBuilder(
-      stream: _controller.stream,
-      builder: (ctx, snapshot) {
-        print('snapshot.hasData ${snapshot.hasData}');
-        if (!snapshot.hasData) {
-          return noContent('snapshot');
+      stream: FirebaseFirestore.instance
+          .collection('photos')
+          .orderBy(
+            'createdAt',
+            descending: true,
+          )
+          .snapshots(),
+      builder: (ctx, photoSnapshot) {
+        if (photoSnapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         }
 
-        List<Reference> lista = snapshot.data!.items;
-        print('length=${lista.length}');
+        if (!photoSnapshot.hasData || photoSnapshot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('No photos found.'),
+          );
+        }
+
+        if (photoSnapshot.hasError) {
+          return const Center(
+            child: Text('Something is wrong with photo database!'),
+          );
+        }
+
+        final loadedPhotosMeta = photoSnapshot.data!.docs;
+        print('snapshot length = ${loadedPhotosMeta.length}');
 
         return PageView.builder(
           scrollDirection: Axis.vertical,
+          itemCount: loadedPhotosMeta.length,
           itemBuilder: (ctx, index) {
-            print('indexer ${_indexer}');
-            if (_indexer >= lista.length) {
-              loadPhotos();
-              return const Center(
-                child: Text('loading'),
-              );
-            }
-            return Ratemy(photoRef: lista[_indexer++]);
+            final photoMeta = loadedPhotosMeta[index].data();
+            return Ratemy(photoMeta: photoMeta);
           },
         );
       },
